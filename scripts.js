2a5cd06fcd83684335ff5959b66b19439205d4ff
hianmateus <hianmateus1@gmail.com>
2024-03-05 21:22:07 -0300

    scripts melhorados

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const myObserver = new IntersectionObserver((entries) => {
-    console.log(entries)
     entries.forEach( (entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
@@ -12,3 +11,31 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach((element) => myObserver.observe(element))
+
+function MoveSection2() {
+    let Section2 = document.querySelector('.Section2')
+
+    Section2.scrollIntoView({ block: 'end' } )
+}
+
+function MoveSection3() {
+    let Section3 = document.querySelector('.Section3')
+
+    Section3.scrollIntoView({ block: 'start' })
+}
+
+function MoveSection4() {
+    let Section4 = document.querySelector('.Section4')
+
+    Section4.scrollIntoView({ block: 'start', top: -100})
+}
+
+
+function MoveSection5() {
+    let Section5 = document.querySelector('.Section5')
+
+    Section5.scrollIntoView({ behavior: 'smooth' })
+}
+
+
+
